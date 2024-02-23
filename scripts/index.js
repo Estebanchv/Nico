fed6f842eb3407fbee8632597fe08bfb93a6e9f2
Estebanchv <Humberto011956@hotmail.com>
2024-02-23 13:11:17 -0500

    otro cambio

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 23, 2024',
+  birthdate: 'Feb 25, 2024',
   name: '❤️Nicoll ❤️'
 };
 
